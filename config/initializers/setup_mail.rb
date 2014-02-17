d069d9312ddf5931bbeb79d2a90b7e01ac1ed746
RafaelSabel <rafael@gett.com.br>
2014-02-17 09:04:32 -0300

    commit final do projeto

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "gmail.com",  
-  :user_name            => "lucasluandemelo@gmail.com",  
-  :password             => "theislander",  
+  :user_name            => "alexandria.biblioteca.no.reply@gmail.com",  
+  :password             => "alexandria18",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
