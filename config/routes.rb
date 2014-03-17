c21fb367e9b68e53d4a48b1acfc3013feef48418
lucasmpereira <lucas@gett.com.br>
2014-03-17 11:20:27 -0300

    Modificação nas rotas do devise para o funcionamento da inserção de um novo usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Alexandria::Application.routes.draw do
-  
-  devise_for :users
 
-  resources :loans
+	devise_for :users, :skip => [:registrations]
+	devise_scope :user do
+  		get "signup",   :to => "accounts#new"
+  		get "signin",   :to => "devise/sessions#new"
+  		get "signout",  :to => "devise/sessions#destroy"
+  		get "cancel_user_registration", :to => "devise/registrations#cancel"
+  		post "user_registration",       :to => "users#create"
+  		get "new_user_registration",    :to => "accounts#new"
+  		get "edit_user_registration",   :to => "users#edit"
+	end
+	
+	resources :users
 
-  resources :queue_lists
+	resources :loans
+	resources :queue_lists
+	resources :books
+	
+	match '/permissions'  => 'home#permissions'
 
-  resources :books
-
-  resources :users
-
-  match '/permissions'  => 'home#permissions'
-
-  root :to => "home#index"
-   
+	root :to => "home#index"
 end
