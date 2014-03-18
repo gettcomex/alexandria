c74a311e400117bed0965e6b0fe6c37a4f934b90
lucasmpereira <lucas@gett.com.br>
2014-03-18 16:54:16 -0300

    Adicionado campo id no model do usuário pois não tinha

--- public/app/model/User.js
+++ public/app/model/User.js
@@ -1,6 +1,7 @@
 Ext.define('AW.model.User', {
 	extend	: 'Ext.data.Model', 
 	fields	: [
+		{name: 'id',					type: 'int'},
 		{name: 'email',					type: 'string'}, 
 		{name: 'password', 				type: 'string'}, 
 		{name: 'password_confirmation',	type: 'string'},
