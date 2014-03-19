ee4d66a906ecdecf9f7bacd6f5a9fd1d8760ea71
lucasmpereira <lucas@gett.com.br>
2014-03-19 15:53:03 -0300

    Adicionado campo returned

--- public/app/model/Loan.js
+++ public/app/model/Loan.js
@@ -7,6 +7,7 @@ Ext.define('AW.model.Loan', {
 		{name: 'book_title', type: 'string'},
 		{name: 'user_id',	type: 'int'},
 		{name: 'user_name', type: 'string'},
+		{name: 'returned',	type: 'boolean'},
 		{name: 'starts_at',	type: 'date',	dateFormat: 'd-m-Y'},
 		{name: 'end_at',	type: 'date',	dateFormat: 'd-m-Y'}
 	]
