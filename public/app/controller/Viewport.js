62a7eb1b4f69ffe53a9f58e38315257b0555e2cd
lucasmpereira <lucas@gett.com.br>
2014-03-28 18:06:32 -0300

    Implementado controller do viewport para ação do botão de logout

---
+++ public/app/controller/Viewport.js
@@ -0,0 +1,29 @@
+/*globals Ext*/
+Ext.define('AW.controller.Viewport', {
+	extend	: 'Ext.app.Controller',
+	views	: [],
+	stores	: [],
+
+//inits
+	init: function() {
+		var me	= this;
+		me.control({
+			'button[action=sign_out]': {
+				click: me.onClickBtnLogout
+			}
+		});
+
+		me.callParent(arguments);
+	},
+
+//listeners list
+	onClickBtnLogout: function(btn) {
+		Ext.Ajax.request({
+			url: '/signout',
+			method: 'GET',
+			success: function(response){
+				window.location.reload()
+			}
+		})
+	}
+});
